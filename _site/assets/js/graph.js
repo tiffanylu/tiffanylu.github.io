752fe9c5fd28865b90bd9de82a8ba5e218e7aaeb
Tiffany Lu <tffnlu@gmail.com>
2017-11-30 02:23:18 -0500

    switch strnegthgt

--- _site/assets/js/graph.js
+++ _site/assets/js/graph.js
@@ -9,8 +9,8 @@ var svg = d3.select("svg")
 var simulation = d3.forceSimulation()
 .force("link", d3.forceLink()
   .id(function(d) { return d.id; })
-  .distance(function(d) {return 85 * d.value}).strength(1))
-.force("charge", d3.forceManyBody().strength(-1000))
+  .distance(function(d) {return 80 * d.value}).strength(1))
+.force("charge", d3.forceManyBody().strength(-1200))
 .force("center", d3.forceCenter(width / 2, height / 2 - 50));
 
 d3.json("data/portfolio-meta.json", function(error, graph) {
